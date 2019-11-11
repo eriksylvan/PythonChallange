1f7e4c4951a7debdec55220c4bb82b2ff7b39211
Erik Sylvan <erik.sylvan@delaval.com>
2019-11-11 23:44:16 +0100

    3 solution

--- 3/3.py
+++ 3/3.py
@@ -2,29 +2,27 @@
 
 f =  open("characters")
 
-upper = 0
-characters = {}
-window = ['1','2', '3','4','5','6','7']
 
 def checkSmallBetween3Big(letters):
-    return letters[0].isupper() and \
+    return letters[0].islower() and \
            letters[1].isupper() and \
            letters[2].isupper() and \
-           letters[3].islower() and \
-           letters[4].isupper() and \
+           letters[3].isupper() and \
+           letters[4].islower() and \
            letters[5].isupper() and \
-           letters[6].isupper()
-
+           letters[6].isupper() and \
+           letters[7].isupper() and \
+           letters[8].islower()
 found = 0
 for line in f:
     # print(len(line))
-    for pos in range(3, len(line)):
-        if checkSmallBetween3Big(line[pos-3:pos + 4]):
-            print(line[pos - 3:pos + 4])
+    for pos in range(4, len(line)-4):
+        if checkSmallBetween3Big(line[pos-4:pos + 5]):
+            #print(line[pos - 4:pos + 5])
             found = found + 1
-            #print(line[pos],end='')
-            print(line[pos])
+            print(line[pos],end='')
+            #print(line[pos])
 
 
 print()
-print(found)
+#print(found)
