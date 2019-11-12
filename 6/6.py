37f0b5d1f2d427def3c59baee7b80570cba378eb
Erik Sylvan <erik.sylvan@delaval.com>
2019-11-12 22:24:58 +0100

    Solution 6

--- 6/6.py
+++ 6/6.py
@@ -1,1 +1,21 @@
 # http://www.pythonchallenge.com/pc/def/channel.html
+
+import zipfile, re
+
+
+
+
+f = zipfile.ZipFile("channel.zip")
+print(f.comment.decode("utf-8"))
+num = '90052'
+while True:
+    content = f.read(num + ".txt").decode("utf-8")
+    print(f.getinfo(num + ".txt").comment.decode("utf-8"),end='')
+    #print(content)
+    match = re.search("Next nothing is (\d+)", content)
+    if match == None:
+        break
+    num = match.group(1)
+
+
+    # HOCKEY
