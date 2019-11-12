31e22e5cf4dcc3b89059de2c107764c8e250d817
Erik Sylvan <erik.sylvan@delaval.com>
2019-11-12 22:52:54 +0100

    Solution 7

--- 7/7.py
+++ 7/7.py
@@ -1,1 +1,24 @@
 # http://www.pythonchallenge.com/pc/def/oxygen.html
+
+# pip install pillow
+
+from PIL import Image
+
+img = Image.open('oxygen.png')
+pix = img.load() # pix is a list of (R,G,B,A), which can be accessed by pix[x,y]
+print(img.size) # (width, height)
+
+
+for i in range(0,629,7):
+    pix = img.getpixel((i,47))
+    if pix[0]==pix[1] and pix[0]==pix[2]:
+        #print(pix)
+        print(chr(pix[0]),end='')
+print()
+
+
+# smart guy, you made it. the next level is [105, 110, 116, 101, 103, 114, 105, 116, 121]
+
+answer = [105, 110, 116, 101, 103, 114, 105, 116, 121]
+for ch in answer:
+    print(chr(ch),end='')
