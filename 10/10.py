39e103b484b3b65d89979fed99aac1e6ef8e305e
Erik Sylvan <erik.sylvan@delaval.com>
2019-11-13 17:01:52 +0100

    Solution 10

--- 10/10.py
+++ 10/10.py
@@ -1,32 +1,41 @@
 # http://www.pythonchallenge.com/pc/return/bull.html
 
 # a = [1, 11, 21, 1211, 111221]
-a = [1]
 
 
-for i in a:
-    print(i)
-    numstr = str(i)
-    print(numstr)
-    print(len(numstr))
+
+
+def nextInSequence(numstr):
+    next = []
+    # print("Sekvens: ",numstr)
+    # print(len(numstr))
     pos = 0
     ch = numstr[pos]
     count = 0
-    next = []
-
     for j in numstr:
         if j == ch:
             count = count + 1
-            print("count: {}_{}".format(count, ch))
-            next.append([count,ch])
+
+
         else:
-            next = ch + str(count)
+            # print("count: {}_{}".format(count, ch))
+            next.append([count, ch])
             count = 1
             ch = j
-            print(next)
+    else:
+        # print("count: {}_{}".format(count, ch))
+        next.append([count, ch])
+    result = ''
+    for e in next:
+        for f in e:
+            result += str(f)
+    return result
 
-    print("---")
-    print(next)
+a=["1"]
+for i in range(31):
+    a.append(nextInSequence(a[i]))
 
+print("Number: ", a[30])
+print("Answer: len(a[30])=", len(a[30]))
 
 
